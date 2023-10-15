d360b15f03c11eec7b2cbe70751c3b890c90cb83
Roman <gremkil@gmail.com>
2023-10-15 03:08:38 +0300

    fea: map editor mode

--- editor/mesages.lua
+++ editor/mesages.lua
@@ -6,4 +6,5 @@ return {
     SET_REMOVE_WAYPOINT_MODE = hash("editor_remove_waypoint_mode"),
     SET_REMOVE_ROUTE_MODE    = hash("editor_remove_route_mode"),
     EXPORT                   = hash("editor_export"),
+    LOAD_MAP                 = hash("editor_load_map")
 }
